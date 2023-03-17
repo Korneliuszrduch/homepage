1b8d5d82605cfe91aa5a9b0c1b3a95132dee09ea
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2023-03-17 20:51:23 +0100

    change name folde

---
+++ js_/script.js
@@ -0,0 +1,28 @@
+
+{
+    const welcome = () => {
+        console.log("Witam wszystkich developerów");
+    };
+    const removeHeader = () => {
+        const button = document.querySelector(".js-button");
+        const header = document.querySelector(".js-header");
+        button.addEventListener("click", () => {
+            header.remove();
+            button.remove();
+        });
+    };
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const nextColorname = document.querySelector(".js-nextColorname");
+        body.classList.toggle("body--grey");
+        nextColorname.innerText = body.className.match("body--grey") ? "jasny" : "ciemny";
+    };
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+    };
+    
+    removeHeader();
+    welcome();
+    init();
+}
