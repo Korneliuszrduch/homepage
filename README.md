79888f76dd2c81a659efe1f5295561788e2e1c0f
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2023-03-23 18:44:33 +0100

    Update Readme end JS

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 ![Korneliusz](https://avatars.githubusercontent.com/u/127408364?v=4)
 ## Demo
 https://korneliuszrduch.github.io/homepage/
+1. HTML
+2. BEM convention
+3. CSS
+4. Flex model
+5. JavaScript (ES6+)
+## Description
+his website was created as a homework assignment during the front-end YouCode course. Currently, it contains some information about me.
 
 
--- js/script.js
+++ js/script.js
@@ -3,25 +3,28 @@
     const welcome = () => {
         console.log("Witam wszystkich developerów");
     };
+
     const removeHeader = () => {
         const button = document.querySelector(".js-button");
         const header = document.querySelector(".js-header");
-        button.addEventListener("click", () => {
-            header.remove();
-            button.remove();
-        });
+        
+        button.addEventListener("click", header.remove, button.remove);
     };
+
     const toggleBackground = () => {
         const body = document.querySelector(".js-body");
-        const nextColorname = document.querySelector(".js-nextColorname");
+        const nextColorName = document.querySelector(".js-nextColorname");
+
         body.classList.toggle("body--grey");
-        nextColorname.innerText = body.className.match("body--grey") ? "jasny" : "ciemny";
+        nextColorName.innerText = body.className.match("body--grey") ? "jasny" : "ciemny";
     };
+
     const init = () => {
         const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+
         changeBackgroundButton.addEventListener("click", toggleBackground);
     };
-    
+
     removeHeader();
     welcome();
     init();
